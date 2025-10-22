f3b48d080c30942a77fc36b7fca458102f8b521d
goingtodkworld-cpu <goingtodkworld@gmail.com>
2025-10-22 12:16:28 +0900

    Update HelloWorld.cs

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("한글한글한글");
+        Debug.Log("한글한글한글 수정수정수정");
     }
 
     // Update is called once per frame
