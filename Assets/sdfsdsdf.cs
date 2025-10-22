48370d83f1a0d339bce958d4355daac7fb734469
sksksk54-gif <sksksk54@naver.com>
2025-10-22 15:38:39 +0900

    dfds

---
+++ Assets/sdfsdsdf.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sdfsdsdf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("fdfds"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
