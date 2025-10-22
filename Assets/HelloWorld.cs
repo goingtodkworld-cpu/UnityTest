06e70e813603ed2c0c8b64dfa11eb8c4dc154a46
sksksk54-gif <sksksk54@naver.com>
2025-10-22 11:53:00 +0900

    성광 251022 11:52

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("한글이 써지네요");
     }
 
     // Update is called once per frame
