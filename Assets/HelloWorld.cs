b56be07e4daaadd7d72cdaf0878d16bbb7453a6a
goingtodkworld-cpu <goingtodkworld@gmail.com>
2025-10-22 12:43:55 +0900

    Merge branch 'main' of https://github.com/goingtodkworld-cpu/UnityTest

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("성광이 병합을 냄");
         Debug.Log("한글한글한글 수정수정수정");
     }
 
