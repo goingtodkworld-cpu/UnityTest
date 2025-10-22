90ac2a6524c87b6d97a9a538f49767f7f9f48d18
sksksk54-gif <sksksk54@naver.com>
2025-10-22 15:42:28 +0900

    Update sdfsdsdf.cs

--- Assets/sdfsdsdf.cs
+++ Assets/sdfsdsdf.cs
@@ -7,7 +7,7 @@ public class sdfsdsdf : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("fdfds"); 
+        Debug.Log("fdfdhgfhggfs"); 
     }
 
     // Update is called once per frame
