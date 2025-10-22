19caa23581af340c22c9c46e86d7178b02f8d06b
goingtodkworld-cpu <goingtodkworld@gmail.com>
2025-10-22 15:51:12 +0900

    아무

---
+++ Assets/ddddddddd.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ddddddddd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("아무거나");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
