4558dbcafaec24aced296b2562c9faa5db905172
sksksk54-gif <sksksk54@naver.com>
2025-10-23 12:05:02 +0900

    dsffsd

---
+++ Assets/dodododododo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dodododododo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("SOSOSOOSO");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
